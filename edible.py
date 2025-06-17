dcc3eb6d908c024f84493837568aa56ce6edbc0e
Mike Klopot <michael.klopot@gmail.com>
2025-06-16 23:41:53 -0600

    Create edible.py
    Wumpuses can eat Edibles

---
+++ edible.py
@@ -0,0 +1,6 @@
+class Edible(object):
+    def __init__(self, name, weight, description="It looks very ordinary.",value=1):
+        self.name = name
+        self.weight = weight
+        self.description = description
+        self.value = value
